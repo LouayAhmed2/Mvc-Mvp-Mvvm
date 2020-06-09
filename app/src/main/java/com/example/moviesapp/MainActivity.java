2028f517a5bd7a271e3fbeb9e5d251303d663857
LouayAhmed2 <65208442+LouayAhmed2@users.noreply.github.com>
2020-06-10 01:05:44 +0200

    -Add Button and viewmodel by (MVC)

--- app/src/main/java/com/example/moviesapp/MainActivity.java
+++ app/src/main/java/com/example/moviesapp/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.moviesapp;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    protected Button MovieBtn;
+    protected TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.setContentView(R.layout.activity_main);
+        initView();
+        MovieBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(getMoviefromDatabase().name);
+            }
+        });
+
+    }
+
+    private void initView() {
+        MovieBtn = (Button) findViewById(R.id.Movie_btn);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public  MovieViewModel getMoviefromDatabase(){
+        return new MovieViewModel("X-men","14/9","Action",1);
     }
 }
