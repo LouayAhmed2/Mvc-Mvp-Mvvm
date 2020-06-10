f3046fd8327f7eeba5172ca7a420197a590d259a
LouayAhmed2 <65208442+LouayAhmed2@users.noreply.github.com>
2020-06-10 02:52:54 +0200

    -add databinding to Design pattern  (MVVM)

--- app/src/main/java/com/example/moviesapp/Ui/MainActivity.java
+++ app/src/main/java/com/example/moviesapp/Ui/MainActivity.java
@@ -1,51 +1,33 @@
 package com.example.moviesapp.Ui;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.moviesapp.R;
+import com.example.moviesapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity   {
 
-    protected Button MovieBtn;
-    protected TextView textView;
+
     MovieViewModel movieViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.activity_main);
-        initView();
-        MovieBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
- movieViewModel.getMovieName();
-            }
-        });
+
+        final ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
         movieViewModel = ViewModelProviders.of(this).get(MovieViewModel.class);
-        movieViewModel.MovieNameMutableLiveData.observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                textView.setText(s);
+        activityMainBinding.setViewmodel(movieViewModel);
+        activityMainBinding.setLifecycleOwner(this);
 
-            }
-        });
 
 
     }
-
-    private void initView() {
-        MovieBtn = (Button) findViewById(R.id.Movie_btn);
-        textView = (TextView) findViewById(R.id.textView);
-    }
-
 
 
 }
